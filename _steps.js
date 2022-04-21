74d9ae27d63d200f65b0d3a6f065d9de25fd10cc
Bablu Mia <bablumiah2000@gmail.com>
2022-04-21 21:59:11 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,44 @@
+// --------------
+/**
+ * ---------------------
+ * Basic server setup
+ * --------------------
+ * 1. create a folder (manually or mkdir)
+ * 2. open command line to that folder
+ * 3. npm init
+ * 4. npm install express cors mongodb
+ * 5. create index.js
+ * 6.use 5 steps to crate node server
+ * --------------------
+ * create atlas account
+ * ------------------
+ * 1.sign up google access
+ * 2.create cluster
+ * 3.create user db and pass
+ * 4.Network access allow all 
+ * 5.database  > conect > copy code and paste in index.js
+ * ---------------------------
+ * CRUD operation 
+ * -----------------------
+ * 1. node mongodb CRUD > fundamentals
+ * 2.create async run function 
+ * ---------------------------
+ * Intregate send data client to server
+ * ----------------------------------
+ * 1.create a from in client site
+ * 2.onSubmit get data and user object
+ * 3.on server create user POST method to recive data on the backend
+ * 4.on clinnt site fetch - with POST header body
+ * 5.make sure you return a jsn from POST api
+ * -----------------------------------------
+ * load data to the client side
+ * 1.create get api
+ * 2.create a query object
+ * 3.collection.find
+ * 4.cursor.toArray()
+ * 5.return the result
+ * 6. from client useEffect() ager moto data display korte hbe
+ * -------------------------------
+ * delete data
+ * --------------------------
+*/
---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require("express")
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json());
+
+// user : userdb1
+// pass : VNK#UQhk3kCpxgT
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { use } = require("express/lib/router");
+const uri = "mongodb+srv://userdb3:asdfg123@cluster0.ifrt9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// async function run (){
+//     try{
+//         await client.connect();
+//         const userCollection = client.db("foodExpress").collection("user");
+//         const user ={name:"nadim" , email:"nadim@gmail.com"}
+//         const result = await userCollection.insertOne(user)
+//         // const result = await userCollection.deleteMany(query);
+//         console.log(`User inserted with id : ${result.insertedId}`);
+//     }
+//     finally{
+//         await client.close()
+//     }
+// }
+
+
+
+
+async function run (){
+    try{
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user")
+        
+        // get users
+        app.get("/user",async (req,res)=>{
+            const query = {};
+            const cursor = userCollection.find(query)
+
+            const users = await cursor.toArray()
+            res.send(users)
+        });
+        // single user
+        app.get("/user/:id", async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send(result)
+        })
+
+        // POST : add a new user
+        app.post("/user" , async (req,res)=>{
+            const newUser = req.body;
+            console.log("adding new user" , newUser);
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+        // update user 
+        app.put('/user/:id',async (req,res)=>{
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set:{
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            }
+            const result = await userCollection.updateOne(filter , updatedDoc , options)
+            res.send(result)
+        })
+        // delete user
+        app.delete("/user/:id" , async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+run().catch(console.dir);
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Running node server")
+})
+
+app.listen(port , ()=>{
+    console.log("CRUD Suerver Is Running..");
+})
